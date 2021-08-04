103c7be6c41b546cfe37df0db4fa65389763664f
malyzke <malyzke>
2021-08-04 13:57:43 +0300

    add styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,25 @@
 import '../scss/style.scss';
 
-const toggleMenu = document.querySelector('.burger-toggle')
-      menu = document.querySelector('.menu')
-      contentMore = document.querySelector('.text-more')
-      closeMenu = document.querySelector('.burger-close')
-      contentBtn = document.querySelector('.content-text__more')
-      learnMore = document.querySelector('.brands-more') // кнопка
-      cartFive = document.querySelectorAll('.five')
-      brandsBtn = document.querySelector('.brands-icon')
-      transformImg = document.querySelector('.text-img')
-      cartBtn = document.querySelector('.cart-more')
-      threeCart = document.querySelectorAll('.three-cart')
-      cartIcon = document.querySelector('.cart-icon')
+const toggleMenu = document.querySelector('.burger-toggle'),
+      menu = document.querySelector('.menu'),
+      contentMore = document.querySelector('.text-more'),
+      closeMenu = document.querySelector('.burger-close'),
+      contentBtn = document.querySelector('.content-text__more'),//btn
+      learnMore = document.querySelector('.brands-more'), // btn
+      cartFive = document.querySelectorAll('.five'),
+      brandsBtn = document.querySelector('.brands-icon'),
+      transformImg = document.querySelector('.text-img'),
+      cartBtn = document.querySelector('.cart-more'), //btn
+      threeCart = document.querySelectorAll('.three-cart'),
+      cartIcon = document.querySelector('.cart-icon'),
+      expendImg = document.querySelector('.text-img'),
+      feedbackBtn = document.querySelectorAll('.repair'),
+      modalFeedback = document.querySelector('.modalfeedback'),
+      closeModalFeedback =  document.querySelector('.modalfeedback-close'),
 
-      feedbackBtn = document.querySelectorAll('.repair')
-      modalFeedback = document.querySelector('.modalfeedback')
-      closeModalFeedback =  document.querySelector('.modalfeedback-close')
-
-      modalCallBtn = document.querySelectorAll('.search')
-      modalCall = document.querySelector('.modalcall')
-      closeModalCall = document.querySelector('.modalcall-close')
+      modalCallBtn = document.querySelectorAll('.search'),
+      modalCall = document.querySelector('.modalcall'),
+      closeModalCall = document.querySelector('.modalcall-close');
 
   console.log(modalCallBtn)
          modalCallBtn.forEach( (item) => {
@@ -50,12 +50,32 @@ const toggleMenu = document.querySelector('.burger-toggle')
          for (let cart of cartFive) {
           cart.classList.toggle('show-cart')
         }
+        if (learnMore.textContent === 'Скрыть') {
+            learnMore.textContent = 'Показать всё'
+            learnMore.prepend(brandsBtn)
+            } else {
+             learnMore.textContent = 'Скрыть'
+             learnMore.prepend(brandsBtn)
+            }
       })
+
+
+
+
       cartBtn.addEventListener('click', () => {
-        cartIcon.classList.toggle('transform-icon')
+        // cartIcon.classList.toggle('transform-icon')
         for (let cart of threeCart) {
           cart.classList.toggle('show-three')
+           cartIcon.classList.toggle('transform-icon')
         }
+         if (cartBtn.textContent === 'Скрыть') {
+            cartBtn.textContent = 'Показать всё'
+            cartBtn.prepend(cartIcon)
+            } else {
+             cartBtn.textContent = 'Скрыть'
+             cartBtn.prepend(cartIcon)
+            }
+
       })
 
        toggleMenu.addEventListener('click', ()=> {
@@ -69,7 +89,18 @@ const toggleMenu = document.querySelector('.burger-toggle')
           contentBtn.addEventListener('click', ()=> {
           contentMore.classList.toggle('show-text')
           transformImg.classList.toggle('transform')
+           if (contentBtn.textContent === 'Скрыть') {
+            contentBtn.textContent = 'Показать всё'
+            contentBtn.prepend(transformImg)
+            } else {
+             contentBtn.textContent = 'Скрыть'
+                         contentBtn.prepend(transformImg)
+
+
+            }
+
       });
+
 
 
 
